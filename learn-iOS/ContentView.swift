0afad825804c128ed644206bc6a84c4a527a5ed8
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-07-12 15:23:40 +0700

    Initial Commit

---
+++ learn-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learn-iOS
+//
+//  Created by Ricky Primayuda Putra on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learn-iOS/learn_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_iOSApp.swift
+//  learn-iOS
+//
+//  Created by Ricky Primayuda Putra on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
